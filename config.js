cf721d28a03178042f5108bf3014bc212ef63709
nivsade <nivsade2@gmail.com>
2026-09-12 19:57:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Development placeholder. Replace with the values from Supabase > Connect / API Keys.
 window.PENSION_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: '',
+  supabaseUrl: 'https://trnlsgwcuiodhmtrbgek.supabase.co',
+  supabasePublishableKey: 'sb_publishable_UrNE-nUB1hNVH9rDBoffug_zGFx5hKu',
   submitFunctionName: 'submit-check'
 };
